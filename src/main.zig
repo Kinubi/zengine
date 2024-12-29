6e4ebb8bf44442485ebe74061bab5d539d8ee170
barend <barend@explore.ai>
2024-12-29 13:04:36 +0200

    Added shaders for triangle

--- src/main.zig
+++ src/main.zig
@@ -17,7 +17,12 @@ pub fn main() !void {
     defer glfw.terminate();
 
     // Create our window
-    const window = glfw.Window.create(1280, 720, "Hello, zengine!", null, null, .{}) orelse {
+    const window = glfw.Window.create(1280, 720, "Hello, zengine!", null, null, .{
+        .context_version_major = gl.info.version_major,
+        .context_version_minor = gl.info.version_minor,
+        .opengl_profile = .opengl_core_profile,
+        .opengl_forward_compat = true,
+    }) orelse {
         std.log.err("failed to create GLFW window: {?s}", .{glfw.getErrorString()});
         std.process.exit(1);
     };
@@ -35,6 +40,59 @@ pub fn main() !void {
         0.5,  -0.5, 0,
         0.0,  0.5,  0,
     };
+
+    //Shaders
+    const vertexShaderFP = "../../res/shaders/simple_shader.vert";
+    const fragShaderFP = "../../res/shaders/simple_shader.frag";
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+
+    const full_vs_path = std.fs.path.join(gpa.allocator(), &.{
+        std.fs.selfExeDirPathAlloc(gpa.allocator()) catch unreachable,
+        vertexShaderFP,
+    }) catch unreachable;
+
+    const full_fs_path = std.fs.path.join(gpa.allocator(), &.{
+        std.fs.selfExeDirPathAlloc(gpa.allocator()) catch unreachable,
+        fragShaderFP,
+    }) catch unreachable;
+
+    std.debug.print("full_vs_path: {s}\n", .{full_vs_path});
+
+    const vs_file = std.fs.openFileAbsolute(full_vs_path, .{}) catch unreachable;
+    const vertexShaderSource = vs_file.readToEndAllocOptions(gpa.allocator(), (10 * 1024), null, @alignOf(u8), 0) catch unreachable;
+
+    const fs_file = std.fs.openFileAbsolute(full_fs_path, .{}) catch unreachable;
+    const fragmentShaderSource = fs_file.readToEndAllocOptions(gpa.allocator(), (10 * 1024), null, @alignOf(u8), 0) catch unreachable;
+
+    const vertexShader = gl.CreateShader(gl.VERTEX_SHADER);
+    gl.ShaderSource(vertexShader, 1, &.{vertexShaderSource.ptr}, null);
+    gl.CompileShader(vertexShader);
+    var success: i32 = undefined;
+    gl.GetShaderiv(vertexShader, gl.COMPILE_STATUS, &success);
+    if (success == 0) {
+        var infoLog: [512]u8 = undefined;
+        gl.GetShaderInfoLog(vertexShader, 512, null, infoLog[0..]);
+        std.log.err("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n{s}\n", .{infoLog});
+    }
+    defer gl.DeleteShader(vertexShader);
+
+    const fragmentShader = gl.CreateShader(gl.FRAGMENT_SHADER);
+    gl.ShaderSource(fragmentShader, 1, &.{fragmentShaderSource.ptr}, null);
+    gl.CompileShader(fragmentShader);
+    gl.GetShaderiv(fragmentShader, gl.COMPILE_STATUS, &success);
+    if (success == 0) {
+        var infoLog: [512]u8 = undefined;
+        gl.GetShaderInfoLog(fragmentShader, 512, null, infoLog[0..]);
+        std.log.err("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n{s}\n", .{infoLog});
+    }
+    defer gl.DeleteShader(fragmentShader);
+
+    const shader = gl.CreateProgram();
+    gl.AttachShader(shader, vertexShader);
+    gl.AttachShader(shader, fragmentShader);
+    gl.LinkProgram(shader);
+    defer gl.DeleteProgram(shader);
 
     var vao: u32 = undefined;
     gl.GenVertexArrays(1, @ptrCast(&vao));
@@ -59,10 +117,9 @@ pub fn main() !void {
         glfw.waitEvents();
         if (window.shouldClose()) break :main_loop;
 
-        // This example draws using only scissor boxes and clearing. No actual shaders!
-
         gl.ClearColor(0.5, 0.1, 0.1, 1);
         gl.Clear(gl.COLOR_BUFFER_BIT);
+        gl.UseProgram(shader);
         gl.BindVertexArray(vao);
         gl.DrawArrays(gl.TRIANGLES, 0, 3);
         window.swapBuffers();
